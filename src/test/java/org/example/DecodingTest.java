40b7c4f6a4a0877d5cd8cb1290804c2b4f5f4a2a
amani <akai@safaricom.co.ke>
2023-04-14 17:59:17 +0300

    added decode test

--- src/test/java/org/example/DecodingTest.java
+++ src/test/java/org/example/DecodingTest.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecodingTest {
 
+    @Test
+    @DisplayName("Testing Decoding Program")
+    void decode() {
+        assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", Decoding.decode("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", 3));
+        assertEquals("Y@#", Decoding.decode("X@#", 1));
+    }
 }
